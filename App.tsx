a89f15f2e67a3a6eb9a79cc070ba75f9ab667df3
HuguesRomain <huguesromainpro@gmail.com>
2021-04-20 11:23:58 +0200

    init react navigation

--- App.tsx
+++ App.tsx
@@ -1,15 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import styled from 'styled-components';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Test>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      </Test>
     </View>
   );
 }
+
+const Test = styled.view`
+  background-color: 'red'
+`
 
 const styles = StyleSheet.create({
   container: {
