d293fa6df59cd77c425d0eaddd641721b72f0a0e
HuguesRomain <huguesromainpro@gmail.com>
2021-04-20 12:36:10 +0200

    styled exemples

--- wonder/services/index.tsx
+++ wonder/services/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { View } from 'react-native';
+import styled from 'styled-components';
 
+
+const ViewSytle = styled.view`
+  background-color: 'red';
+`
 
 // Remove this component to add services
 
 export const Servies = () => {
   return (
-    <View></View>
+    <ViewSytle></ViewSytle>
   );
 }
 
